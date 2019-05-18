b4bf8660c492289f2672781c68661d338cb8340c
Eric <ealbor@my.wctc.edu>
2019-05-17 23:58:55 -0500

    NorthWind .Net Core Class final Project Submission
    This is what was turned in for the final.
    Still need to work on other features.

---
+++ .Net-core-Final-Project/NorthwindBase/NorthwindBaseProject/Controllers/ReportsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class ReportsController : Controller
+    {
+
+        //This controller will depend on the NORTHWIND REPOSITORY
+        private INorthwindRepository repository;
+        public ReportsController(INorthwindRepository repo) => repository = repo;
+
+
+
+        //this is the IActionResult associated with the link in the nav bar. 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        //This is the route that I'm using for my final project
+        //Custom query returns customers by country
+        [HttpGet, Route("reports/globalCustomers")]
+        public IEnumerable<CountryCustomers> GetCustomerCountries()
+        {
+            return repository.Customers.GroupBy(c => c.Country).Select(grp => new CountryCustomers
+            {
+                Country = grp.Key,
+                NumCompanies = grp.Count()
+            }).OrderByDescending(g => g.NumCompanies);
+        }
+
+
+        //This route isn't needed at all.
+        //The data should only be sent one time to the front end,  and then manupulated from there.
+        //By relying on multi routes, we're expecting the server to be immune to failure, which is not smart. 
+        //Send the data once, then manupulate it there with the route above 
+
+
+        //Riley's Route
+        // returns all products
+
+
+            //Used to return all products
+        [HttpGet, Route("reports/allProducts")]
+        public IEnumerable<Product> Get() => repository.Products.OrderBy(p => p.ProductId);
+
+
+        public IActionResult CategoryBreakdown() => View(repository.Categories.OrderBy(c => c.CategoryId));
+
+
+
+
+        //Custom query returns customers by country by name
+        [HttpGet, Route("reports/globalCustomers/{name}")]
+        public CountryCustomers GetCountryCustomers(String name)
+        {
+            int numCustomers = repository.Customers.Where(c => c.Country.ToLower() == name.ToLower()).Count();
+
+            return new CountryCustomers
+            {
+                Country = name,
+                NumCompanies = numCustomers
+            };
+        }
+    }
+}
---
+++ .Net-core-Final-Project/NorthwindBase/NorthwindBaseProject/Models/CustomerCountries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.Models
+{
+    public class CountryCustomers
+    {
+
+        public String Country { get; set; }
+        public int NumCompanies { get; set; }
+    }
+}
